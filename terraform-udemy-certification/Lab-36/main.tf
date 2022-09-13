118b1af3d07ddf412aef5a4cf63b3d556410b44e
Amadeus Amorim <amorim.amadeus@gmail.com>
2022-09-13 15:26:27 -0300

    Módulos para Multiplas Contas/Regiões

---
+++ terraform-udemy-certification/Lab-36/main.tf
@@ -0,0 +1,33 @@
+
+provider "aws" { // My Root Account
+  region = "us-west-2"
+}
+
+provider "aws" { // My DEV Account
+  region = "us-west-1"
+  alias  = "DEV"
+
+  assume_role {
+    role_arn = "arn:aws:iam::639130796919:role/TerraformRole"
+  }
+}
+
+provider "aws" { // My PROD Account
+  region = "ca-central-1"
+  alias  = "PROD"
+
+  assume_role {
+    role_arn = "arn:aws:iam::032823347814:role/TerraformRole"
+  }
+}
+#--------------------------------------------------------------
+
+module "servers" {
+  source        = "./module_servers"
+  instance_type = "t3.small"
+  providers = {
+    aws.root = aws
+    aws.prod = aws.PROD
+    aws.dev  = aws.DEV
+  }
+}
