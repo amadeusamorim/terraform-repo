865b4a5367460f20a5d4c123be60e60ceced668a
Amadeus Amorim <amorim.amadeus@gmail.com>
2022-09-14 13:47:29 -0300

    Terraforms Logs

---
+++ terraform-udemy-certification/Lab-32/main.tf
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+#
+# Terraform Logs
+# Log Levels: TRACE, DEBUG, INFO, WARN, ERROR
+# Enable Logs:
+#   export TF_LOG=ERROR
+#   export TF_LOG_PATH=terraform.logs
+#
+# Disable Logs:
+#   unset TF_LOG
+#   unset TF_LOG_PATH
+#
+#-------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c9bfc21ac5bf10eb"
+  instance_type = "t3.micro"
+}
