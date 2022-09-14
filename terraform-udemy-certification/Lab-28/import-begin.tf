7e1bbd41f81c5e3da250d3d31e34a58532caca4f
Amadeus Amorim <amorim.amadeus@gmail.com>
2022-09-13 23:20:25 -0300

    terraform import (Importando recursos)

---
+++ terraform-udemy-certification/Lab-28/import-begin.tf
@@ -0,0 +1,25 @@
+# Importar um recurso existente no Terraform começa assim
+#    1. terraform init
+#    2. terraform import aws_instance.node1 i-01a1c2b58e21fa64c
+#    3. terraform import aws_instance.node2 i-01d039a8106a24d19
+#    4. terraform import aws_instance.node3 i-065d5e431adf1266a
+#    5. terraform import aws_security_group.k8s sg-0e1bf8ce83c3f21b2
+#-------------------------------------------------------------------------------
+
+# O terraform plan não vai fazer nada
+
+resource "aws_instance" "node1" {
+
+}
+
+resource "aws_instance" "node2" {
+
+}
+
+resource "aws_instance" "node3" {
+    
+}
+
+resource "aws_security_group" "k8s" { // As vezes é necessário adicionar a descrição para integrar corretamente.
+
+}
